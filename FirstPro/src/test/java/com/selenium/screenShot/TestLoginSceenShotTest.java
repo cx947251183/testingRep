b2333d9529c5ee74275279418144a9e91e3715aa
Administrator <Administrator@aa017>
2018-07-29 10:39:18 +0800

    third

---
+++ FirstPro/src/test/java/com/selenium/screenShot/TestLoginSceenShotTest.java
@@ -0,0 +1,11 @@
+package com.selenium.screenShot;
+
+import org.testng.annotations.Test;
+
+public class TestLoginSceenShotTest {
+
+  @Test
+  public void test() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
